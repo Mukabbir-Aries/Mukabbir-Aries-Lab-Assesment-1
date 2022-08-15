8f4cf702c6126064233fda57cfe10af3f489e604
Mukabbir-Aries <101591994+Mukabbir-Aries@users.noreply.github.com>
2022-08-15 22:11:32 +0600

    Update Q5.c

--- Q5.c
+++ Q5.c
@@ -4,17 +4,18 @@
 #define jan 31
 //Mukabbir Hossain 221-35-974(F)//
 int main(){
-int a;
+char ch[100];
 float c,d,sum;
 p("Enter Employee's ID\n");
-s("%d",&a);
+s("%s",&ch);
 p("Enter Employee's work hour in a month\n");
 s("%f",&c);
 p("Enter Amount he receives per hour\n");
 s("%f",&d);
 sum=c*d*jan;
-p("The Employee's ID is %d \n",a);
+p("The Employee's ID is %s \n",ch);
 p("The Employee's work hour in a month is %.2f\n",c);
 p("The Employee's receives per hour is %.2f\n",d);
 p("The Employee's salary of the January month is %.2f \n",sum);
 return 0;}
+
